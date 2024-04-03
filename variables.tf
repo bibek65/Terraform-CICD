b46a9605d247332d6e8830ca80c8cfad581f888a
bibek65 <bkarna65@gmail.com>
2024-04-03 23:11:51 +0545

    Bootstrap Terraform S3 backend Create S3 Bucket Frontend

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "tf_backend_bucket_name" {
+  default = "lf-devops-gitops-terraform-state"
+}
+
+
+variable "bucket_name" {
+  type    = string
+  default = "bibek-s3-bucket"
+}
